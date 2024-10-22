d0d8caf83132636148a68d0c6d0c7eafb7b61c7d
Stephen Molloy <sdmolloy@gmail.com>
2024-10-22 17:17:23 +0200

    First commit! Stringviews and dynamic arrays!

---
+++ src/sdm_lib.h
@@ -0,0 +1,51 @@
+#ifndef _SDM_LIB_H
+#define _SDM_LIB_H
+
+#include <stdlib.h>
+
+#define ARRAY_PUSH(da, item) do {                                   \
+    if (((da)->capacity == 0) || ((da)->data == NULL)) {            \
+      (da)->capacity = DEFAULT_CAPACITY;                            \
+      (da)->data = calloc((da)->capacity, sizeof((da)->data[0]));   \
+      if ((da)->data == NULL) {                                     \
+        fprintf(stderr, "Could not alloc memory. Quitting.\n");     \
+        exit(1);                                                    \
+      }                                                             \
+    }                                                               \
+    while ((da)->length >= (da)->capacity) {                        \
+      (da)->capacity *= 2;                                          \
+      (da)->data = realloc((da)->data,                              \
+                           (da)->capacity * sizeof((da)->data[0])); \
+    }                                                               \
+    (da)->data[(da)->length++] = item;                              \
+  } while (0);
+
+#define ARRAY_FREE(da) do { \
+    free((da).data);        \
+    (da).length = 0;        \
+    (da).capacity = 0;      \
+  } while (0);
+
+#define ARRAY_RESET(da) do { \
+    (da).length = 0;         \
+  } while (0)
+
+typedef struct {
+  size_t length;
+  char *data;
+} SDM_StringView;
+
+#define SDM_SV_F "%.*s"
+#define SDM_SV_Vals(S) (int)(S).length, (S).data
+
+char *SDM_read_entire_file(const char *file_path);
+
+SDM_StringView SDM_cstr_as_sv(char *cstr);
+SDM_StringView SDM_sized_str_as_sv(char *cstr, size_t length);
+
+SDM_StringView SDM_sv_pop_by_delim(SDM_StringView *SV, const char delim);
+void SDM_sv_trim(SDM_StringView *SV);
+
+
+#endif /* ifndef _SDM_LIB_H */
+
---
+++ src/test.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "sdm_lib.h"
+
+#define DEFAULT_CAPACITY 128
+
+typedef struct {
+  double *data;
+  size_t length;
+  size_t capacity;
+} double_da_array;
+
+typedef struct {
+  char *data;
+  size_t length;
+  size_t capacity;
+} double_char_array;
+
+int main(void) { const char *file_path = "./src/test.c";
+  char *contents = SDM_read_entire_file(file_path);
+  printf("%s\n", contents);
+  free(contents);
+
+  char *multi_line_str = "Hello\nWorld\nBlahblah\n";
+  SDM_StringView multi_line_sv = SDM_cstr_as_sv(multi_line_str);
+  SDM_StringView line_1 = SDM_sv_pop_by_delim(&multi_line_sv, '\n');
+  SDM_StringView line_2 = SDM_sv_pop_by_delim(&multi_line_sv, '\n');
+  printf("Line 1: "SDM_SV_F"\n", SDM_SV_Vals(line_1));
+  printf("Line 2: "SDM_SV_F"\n", SDM_SV_Vals(line_2));
+
+  char *test_trim = "   Hello!";
+  SDM_StringView test_trim_sv = SDM_cstr_as_sv(test_trim);
+  SDM_sv_trim(&test_trim_sv);
+  printf("\n");
+  printf("Untrimmed string = %s\n", test_trim);
+  printf("Trimmed string = "SDM_SV_F"\n", SDM_SV_Vals(test_trim_sv));
+
+  printf("\nCreating empty dyn array...\n");
+  double_da_array dbl_da = {0};
+  printf("dbl_da has a length of %zu\n", dbl_da.length);
+  printf("dbl_da has a capacity of %zu\n", dbl_da.capacity);
+  for (size_t i=0; i<dbl_da.length; i++) {
+    printf("Item %zu: %f\n", i, dbl_da.data[i]);
+  }
+
+  printf("\nPushing...\n");
+  ARRAY_PUSH(&dbl_da, 1.0);
+  ARRAY_PUSH(&dbl_da, 2.0);
+  printf("dbl_da has a length of %zu\n", dbl_da.length);
+  printf("dbl_da has a capacity of %zu\n", dbl_da.capacity);
+  for (size_t i=0; i<dbl_da.length; i++) {
+    printf("Item %zu: %f\n", i, dbl_da.data[i]);
+  }
+
+  printf("\nResetting...\n");
+  ARRAY_RESET(dbl_da);
+  printf("dbl_da has a length of %zu\n", dbl_da.length);
+  printf("dbl_da has a capacity of %zu\n", dbl_da.capacity);
+  for (size_t i=0; i<dbl_da.length; i++) {
+    printf("Item %zu: %f\n", i, dbl_da.data[i]);
+  }
+
+  printf("\nFreeing...\n");
+  ARRAY_FREE(dbl_da);
+  printf("dbl_da has a length of %zu\n", dbl_da.length);
+  printf("dbl_da has a capacity of %zu\n", dbl_da.capacity);
+  for (size_t i=0; i<dbl_da.length; i++) {
+    printf("Item %zu: %f\n", i, dbl_da.data[i]);
+  }
+
+  printf("\nCreating another empty dyn array...\n");
+  double_char_array dbl_char = {0};
+  printf("dbl_char has a length of %zu\n", dbl_char.length);
+  printf("dbl_char has a capacity of %zu\n", dbl_char.capacity);
+  for (size_t i=0; i<dbl_char.length; i++) {
+    printf("Item %zu: %c\n", i, dbl_char.data[i]);
+  }
+
+  printf("\nPushing...\n");
+  ARRAY_PUSH(&dbl_char, 'A');
+  ARRAY_PUSH(&dbl_char, 'B');
+  ARRAY_PUSH(&dbl_char, 'C');
+  printf("dbl_char has a length of %zu\n", dbl_char.length);
+  printf("dbl_char has a capacity of %zu\n", dbl_char.capacity);
+  for (size_t i=0; i<dbl_char.length; i++) {
+    printf("Item %zu: %c\n", i, dbl_char.data[i]);
+  }
+
+  return 0;
+}
